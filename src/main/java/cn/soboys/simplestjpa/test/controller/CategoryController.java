3453d884fc1b78e9349b15c88fc9879e92424490
kenx <17521197955@163.com>
2023-08-08 22:24:11 +0800

    初始化模板依赖

---
+++ src/main/java/cn/soboys/simplestjpa/test/controller/CategoryController.java
@@ -0,0 +1,12 @@
+package cn.soboys.simplestjpa.test.controller;
+
+/**
+ * @author 公众号 程序员三时
+ * @version 1.0
+ * @date 2023/8/8 21:49
+ * @webSite https://github.com/coder-amiao
+ */
+public class CategoryController {
+
+
+}
